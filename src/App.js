eab07e350bf22e76115aab8ecfeae6b5250cc39b
jtwray <tuckerwray84@gmail.com>
2019-08-28 21:46:42 -0400

    Initial Commit sets up reactstrap and styled-componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Alert, Spinner} from 'reactstrap'
+import styled from 'styled-components'
+
+const CustomButotn=styled.button( {
+  background: 'navy',
+  color: 'white',
+  border: 'none',
+  borderRadius: 4,
+  padding: '10px 15px',
+  '&hover': {opacity: 0.7}
+} )
+
+const CustomButton=styled.button` 
+background: navy,
+color: white,
+border: none,
+borderRadius: 4,
+padding: 10px 15px,
+&:hover{opacity: 0.7}
+${( props ) => props.size==='small'? 'padding:0px':null};
+${( props ) => props.size==='medium'? 'padding:10px 15px':null};
+${(props) => props.size==='large'? 'padding:30px' : null};
+`
 
 function App() {
   return (
     <div className="App">
+      <Button color="danger">Danger</Button>
+      <Alert color="danger">
+      <Spinner color="danger" size="med"/>This is Alerting YOU...ALERDDD</Alert>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <CustomButotn />
+        <CustomButton size={'large'}>CustomButton</CustomButton>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
